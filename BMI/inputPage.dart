f9dbb6a3f442766f27d1143e4f1aacb76a2d6387
TapasDash <tapasdash017@gmail.com>
2021-05-02 14:09:17 +0530

    Create inputPage.dart

---
+++ BMI/inputPage.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+
+class InputPage extends StatefulWidget {
+  InputPage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+  
+    return Scaffold(
+      appBar: AppBar(
+       
+        title: Text(widget.title),
+        centerTitle: true,
+      ),
+      body: Center(
+      
+        child: Column(
+  
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+              style: Theme.of(context).textTheme.bodyText1,
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
